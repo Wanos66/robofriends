f906c5f1534cf0717cf7381a7062bdb2915b6849
wanos66 <wayne.yallop66@gmail.com>
2020-10-24 18:36:29 +0100

    Changed App.js from class to function

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,54 +1,46 @@
-import React, {Component} from "react";
-import CardList from "../components/CardList";
-import SearchBox from "../components/SearchBox";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./App.css";
-import Scroll from "../components/Scroll";
-import ErrorBoundry from "../components/ErrorBoundry";
+import React, { useState, useEffect } from 'react';
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import Scroll from '../components/Scroll';
+import ErrorBoundry from '../components/ErrorBoundry';
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      robots: [],
-      searchField: "",
-    };
-  }
+function App() {
+  const [robots, setRobots] = useState([]);
+  const [searchField, setSearchField] = useState('');
 
-  componentDidMount() {
-    fetch("https://jsonplaceholder.typicode.com/users")
-        .then((response) => {
-          return response.json();
-        })
-        .then((users) => {
-          this.setState({robots: users});
-        });
-  }
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => {
+        return response.json();
+      })
+      .then((users) => {
+        setRobots(users);
+      });
+  }, []);
 
-  onSearchChange = (event) => {
-    this.setState({searchField: event.target.value});
+  const onSearchChange = (event) => {
+    setSearchField(event.target.value);
   };
 
-  render() {
-    const {robots, searchField} = this.state;
-    const filterRobots = robots.filter((robot) => {
-      return robot.name.toLowerCase().includes(searchField.toLowerCase());
-    });
-    if (!robots.length) {
-      return <h1>Loading...</h1>;
-    } else {
-      return (
-          <div className="text-center">
-            <h1 className="mt-4">RoboFriends</h1>
-            <SearchBox searchChange={this.onSearchChange}/>
-            <Scroll>
-              <ErrorBoundry>
-                <CardList robots={filterRobots}/>
-              </ErrorBoundry>
-            </Scroll>
-          </div>
-      );
-    }
+  const filterRobots = robots.filter((robot) => {
+    return robot.name.toLowerCase().includes(searchField.toLowerCase());
+  });
+  if (!robots.length) {
+    return <h1>Loading...</h1>;
+  } else {
+    return (
+      <div className='text-center'>
+        <h1 className='mt-4'>RoboFriends</h1>
+        <SearchBox searchChange={onSearchChange} />
+        <Scroll>
+          <ErrorBoundry>
+            <CardList robots={filterRobots} />
+          </ErrorBoundry>
+        </Scroll>
+      </div>
+    );
   }
 }
 
